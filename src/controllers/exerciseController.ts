af2088510f18ea9fd9c6841d993a64d20a7c21d3
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2024-07-07 20:56:01 -0300

    feat: add get exercises and gat exercise by id controller

--- src/controllers/exerciseController.ts
+++ src/controllers/exerciseController.ts
@@ -24,5 +24,25 @@ const RegisterExercise = async(req: Request, res: Response) => {
     return res.status(500).json({ error: "Internal server error" });
   }
 }
+const getExercises = async(req: Request, res: Response) => {
+  try {
+    const exercises = await Exercise.find();
+    return res.status(200).json({ exercises });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
+const getExerciseById = async(req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const exercise = await Exercise.findById(id);
+    if (!exercise) {
+      return res.status(404).json({ message: "Exercise not found" });
+    }
+    return res.status(200).json({ exercise });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
 
-export { RegisterExercise }
+export { RegisterExercise, getExercises, getExerciseById }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { deleteUser, edit, login, profile, register } from "../controllers/authController";
 import auth from "../middleware/auth";
-import { RegisterExercise } from "../controllers/exerciseController";
+import { getExerciseById, getExercises, RegisterExercise } from "../controllers/exerciseController";
 
 export const routes = Router()
 
@@ -14,3 +14,5 @@ routes.delete("/delete/:id", auth, deleteUser)
 // EXERCISE ROUTES
 
 routes.post("/exercise", auth, RegisterExercise)
+routes.get("/exercises", auth, getExercises)
+routes.get("/exercise/:id", auth, getExerciseById)
